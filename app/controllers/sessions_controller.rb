a4fbdf681ac9f8e4c88b9222609e78cce7d6df76
hadad <hadadus@gmail.com>
2020-01-22 19:56:35 +0200

    fix(auth): Refactor user sign in
    - put back devise configuration to return appropriate message
    - add test for the endpoint
    
    [Fixes #MRA-38]

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 class SessionsController < Devise::SessionsController
-  respond_to :json
-  skip_before_action :verify_authenticity_token
-
   private
     def respond_with(resource, _opts = {})
       render json: resource
